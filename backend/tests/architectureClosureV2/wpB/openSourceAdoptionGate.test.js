d4fe695967e838265822c1532b18c4090a832e88
来钱 <laiqian0239@gmail.com>
2026-08-03 19:03:56 +0700

    test(wp-b): add RED application governance packaging contract

--- backend/tests/architectureClosureV2/wpB/openSourceAdoptionGate.test.js
+++ backend/tests/architectureClosureV2/wpB/openSourceAdoptionGate.test.js
@@ -5,6 +5,7 @@ require('./xstateUpstreamEvidenceGate.test');
 require('./xstateUpstreamCoreEvidenceContract.test');
 require('./xstateUpstreamCrossPlatform.test');
 require('./xstateSupplyChainBinding.test');
+require('./governancePackagingContract.test');
 require('./xstateAdapterRedEvidence.test');
 require('./xstateAdapterGreenEvidence.test');
 require('./xstateFaultRedEvidence.test');
