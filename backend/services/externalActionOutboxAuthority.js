bdb7eb60154ec383104adf8b04cebfbbe2bfdad6
来钱 <laiqian0239@gmail.com>
2026-08-04 04:35:22 +0700

    fix(wp-b): preserve sealed outbox source bindings

--- backend/services/externalActionOutboxAuthority.js
+++ backend/services/externalActionOutboxAuthority.js
@@ -4,6 +4,14 @@ const core = require('./externalActionOutboxAuthorityCore');
 
 const externalActionOutboxAuthority = new core.ExternalActionOutboxAuthority();
 
+function intentContentSha256(input = {}) {
+  return core.normalizeIntentCommand(input).intentContentSha256;
+}
+
+function recordLateResult(input = {}) {
+  return externalActionOutboxAuthority.recordLateResult(input);
+}
+
 module.exports = externalActionOutboxAuthority;
 for (const [name, value] of Object.entries(core)) {
   Object.defineProperty(module.exports, name, {
@@ -13,4 +21,18 @@ for (const [name, value] of Object.entries(core)) {
     configurable: false
   });
 }
+Object.defineProperties(module.exports, {
+  intentContentSha256: {
+    value: intentContentSha256,
+    enumerable: true,
+    writable: false,
+    configurable: false
+  },
+  recordLateResult: {
+    value: recordLateResult,
+    enumerable: true,
+    writable: false,
+    configurable: false
+  }
+});
 Object.freeze(module.exports);
