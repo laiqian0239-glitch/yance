184b0e2fd516a97b7f2ca6aa7367d646a6b5d251
来钱 <laiqian0239@gmail.com>
2026-08-03 14:59:36 +0700

    test(wp-b): include XState fault RED evidence gate

--- backend/tests/architectureClosureV2/wpB/openSourceAdoptionGate.test.js
+++ backend/tests/architectureClosureV2/wpB/openSourceAdoptionGate.test.js
@@ -5,3 +5,4 @@ require('./xstateUpstreamEvidenceGate.test');
 require('./xstateSupplyChainBinding.test');
 require('./xstateAdapterRedEvidence.test');
 require('./xstateAdapterGreenEvidence.test');
+require('./xstateFaultRedEvidence.test');
