ca01669408718a6c5c735e19b1e7669cd169c765
来钱 <laiqian0239@gmail.com>
2026-08-15 10:49:28 +0700

    feat(v21): inject public Product navigation seam

--- integration/element-module/src/YanceWorkspace.tsx
+++ integration/element-module/src/YanceWorkspace.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { ProductExperienceShell } from "./product-experience/ProductExperienceShell";
+import type { RelationshipProjection } from "./product-experience/experienceTypes";
 
-export function YanceWorkspace(): React.JSX.Element {
-  return <ProductExperienceShell />;
+type YanceWorkspaceProps = {
+  navigateSearchResult?: (relationship: RelationshipProjection) => Promise<boolean>;
+};
+
+export function YanceWorkspace({ navigateSearchResult }: YanceWorkspaceProps): React.JSX.Element {
+  return <ProductExperienceShell navigateSearchResult={navigateSearchResult} />;
 }
