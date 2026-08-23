b5b60e94968c99f688c533cb3898a5eba4aa22d1
来钱 <laiqian0239@gmail.com>
2026-08-24 04:06:20 +0700

    chore: pin full reviewed Electron dependency graph

--- .github/tmp-v21-electron-pin-reviewed-graph.js
+++ .github/tmp-v21-electron-pin-reviewed-graph.js
@@ -12,7 +12,16 @@ pkg.overrides = {
   ...(pkg.overrides || {}),
   electron: {
     ...(electronOverride || {}),
-    '@electron-internal/extract-zip': '1.0.3'
+    '@electron-internal/extract-zip': '1.0.3',
+    '@electron/get': {
+      '.': '5.0.0',
+      'env-paths': '3.0.0',
+      'undici': '7.25.0'
+    },
+    '@types/node': {
+      '.': '24.10.13',
+      'undici-types': '7.16.0'
+    }
   }
 };
 fs.writeFileSync(file, `${JSON.stringify(pkg, null, 2)}\n`, 'utf8');
